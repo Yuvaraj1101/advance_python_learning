48da312b192d5b9fcd7ac706443f7622c99959d1
yuvaraj <yuvarajm1101@gmail.com>
2026-06-29 11:33:01 +0530

    this for che kcing a reset

--- fortry.py
+++ fortry.py
@@ -1,1 +1,7 @@
-#hey there i am try to learn git heyy there this summa 
+#h i also remover everthing thi s commit
+hrrry thrre 
+jnvjbvkr
+v jnfvnl
+v iknmf  ru good oi am juxt keep spaming thingd here 
+f jnv
+heyy hello tehre 
